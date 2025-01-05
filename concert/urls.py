3988475737b41f1e5eaa3bf3ee8f02328c4f3d1e
CloudyJumpy19 <tagangross19@gmail.com>
2025-01-05 13:34:12 -0500

    implemented django application

--- concert/urls.py
+++ concert/urls.py
@@ -3,12 +3,13 @@ from . import views
 
 urlpatterns = [
     re_path(r"^$", views.index, name="index"),
-    path("", views.songs, name="songs"),
-    path("", views.photos, name="photos"),
-    path("", views.login_view, name="login"),
-    path("", views.logout_view, name="logout"),
-    path("", views.signup, name="signup"),
-    path("", views.concerts, name="concerts"),
+    path("song/", views.songs, name="songs"),  # Changed from empty string to "song/"
+    path("photos/", views.photos, name="photos"),  # Updated this line to include "photos/"
+    path("signup/", views.signup, name="signup"),  # Updated this line to include "signup/"
+    path("login/", views.login_view, name="login"),  # Updated login URL
+    path("logout/", views.logout_view, name="logout"),  # Updated logout URL
+    path("concert/", views.concerts, name="concerts"),  # Updated to "concert/" instead of empty string
     path("concert-detail/<int:id>", views.concert_detail, name="concert_detail"),
     path("concert_attendee/", views.concert_attendee, name="concert_attendee"),
 ]
+
